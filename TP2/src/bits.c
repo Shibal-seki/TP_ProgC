f83c6b220adfc7ec24e75b0ca85451244847c982
Shibal-seki <dwaynevallade97293@gmail.com>
2025-04-30 15:16:43 +0200

    Update bits.c

--- TP2/src/bits.c
+++ TP2/src/bits.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    unsigned int d = 0x10000010; // Exemple de valeur (modifiable)
+    unsigned int d = 0x10000010; /// Exemple de valeur (modifiable)
 
     // Calcul des positions : 4e bit de gauche = bit 28, 20e de gauche = bit 12
     int bit_4_gauche = (d >> 28) & 1;   // masque le bit 28
