9d3942febe0148b272a53e07659f5278feeced56
Shibal-seki <dwaynevallade97293@gmail.com>
2025-05-14 16:44:08 +0200

    Update repertoire.c

---
+++ TP5/src/repertoire.c
@@ -0,0 +1,21 @@
+// repertoire.c
+#include "repertoire.h"
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+
+void lire_dossier(const char *nom_repertoire) {
+    DIR *rep = opendir(nom_repertoire);
+
+    if (rep == NULL) {
+        perror("Erreur lors de l'ouverture du répertoire");
+        return;
+    }
+
+    struct dirent *entree;
+    while ((entree = readdir(rep)) != NULL) {
+        printf("%s\n", entree->d_name);
+    }
+
+    closedir(rep);
+}
