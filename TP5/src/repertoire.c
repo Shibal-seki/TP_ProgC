83d1a068a3ef53dfd06dcdb7d1d777b3c600aaae
Shibal-seki <dwaynevallade97293@gmail.com>
2025-05-14 16:50:35 +0200

    Update repertoire.c

--- TP5/src/repertoire.c
+++ TP5/src/repertoire.c
@@ -4,6 +4,7 @@
 #include <dirent.h>
 #include <stdlib.h>
 
+// Fonction qui lit le contenu d'un répertoire
 void lire_dossier(const char *nom_repertoire) {
     DIR *rep = opendir(nom_repertoire);
 
@@ -18,4 +19,17 @@ void lire_dossier(const char *nom_repertoire) {
     }
 
     closedir(rep);
+}
+
+// Fonction main intégrée ici
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Utilisation : %s <nom_du_repertoire>\n", argv[0]);
+        return 1;
+    }
+
+    char *nom_repertoire = argv[1];
+    lire_dossier(nom_repertoire);
+
+    return 0;
 }
